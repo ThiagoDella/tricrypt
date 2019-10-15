59a7dde0b7002ce35b690fbc8e33a0c9c11b161b
hegde5 <vinayhegde2010@gmail.com>
2019-10-15 23:18:56 +0200

    Encrypt folders recursively

--- strategies/decrypt.js
+++ strategies/decrypt.js
@@ -1,17 +1,14 @@
 const chalk = require('chalk');
 const singleFileStrategy = require('./singleFile');
 
-function decryptStrategy(input, output, cipher, recursive) {
-  // Dealing with a single file
-  if (!recursive) {
+function decryptStrategy(input, output, cipher) {
     console.log('\n\n\t' + chalk.white.bgCyan.bold('File') + ' identified, applying ' + chalk.white.bgCyan.bold('single file strategy'));
     singleFileStrategy(input, output, cipher, 'decrypt');  
-  }
 }
 
 
 function init (input, output, cipher, recursive) {
-  return new decryptStrategy(input, output, cipher, recursive);
+  return new decryptStrategy(input, output, cipher);
 }
 
 module.exports = init;
--- strategies/singleFile.js
+++ strategies/singleFile.js
@@ -1,35 +1,58 @@
 const fs = require('fs');
-const path = require('path');
+const path_mod = require('path');
 const tricrypt = require('../tricrypt/tricrypt');
+const readdirp = require('readdirp');
 
-function singleFileStrategy (input, output, cipher, command) {
-  if (!input || !output) 
+
+function singleFileStrategy(input, output, cipher, command) {
+  if (!input || !output)
     throw new Error('Please provide an input path and an output path');
   else {
-    const parsedInput = this.normalizePath(input);
-    const parsedOutput = this.normalizePath(output);
+    const parsedInput = normalizePath(input);
+    const parsedOutput = normalizePath(output);
 
-    const inputExists = this.checkFile(parsedInput);
+    const inputIsFile = this.checkFile(parsedInput);
+    const inputIsDir = this.checkDir(parsedInput);
+
     const outputExists = this.checkDir(parsedOutput);
 
-    const fileName = path.basename(parsedInput);
-    
-    if (inputExists && outputExists && command === 'encrypt') {
+    if (inputIsFile && outputExists && command === 'encrypt') {
+      const fileName = path_mod.basename(parsedInput);
       let enc = tricrypt('aes-256-cbc', cipher);
-      enc.encryptFile(input, output, fileName);      
-    } 
-    else if (inputExists && outputExists && command === 'decrypt') {
+      enc.encryptFile(input, output, fileName);
+    }
+    else if (inputIsFile && outputExists && command === 'decrypt') {
+      const fileName = path_mod.basename(parsedInput);
       let dec = tricrypt('aes-256-cbc', cipher);
-      dec.decryptFile(input, output, fileName);      
-    } else {
+      dec.decryptFile(input, output, fileName);
+    }
+    else if (inputIsDir && outputExists && command === 'encrypt') {
+      let enc = tricrypt('aes-256-cbc', cipher);
+      readdirp(parsedInput, { alwaysStat: true })
+      .on('data', (entry) => {
+        const { path } = entry;
+        const fileName = path_mod.basename(normalizePath(path));
+        enc.encryptFile(path_mod.join(parsedInput, fileName), parsedOutput, fileName)
+      })
+    }
+    else if (inputIsDir && outputExists && command === 'decrypt') {
+      let dec = tricrypt('aes-256-cbc', cipher);
+      readdirp(parsedInput, { alwaysStat: true })
+      .on('data', (entry) => {
+        const { path } = entry;
+        const fileName = path_mod.basename(normalizePath(path));
+        dec.decryptFile(path_mod.join(parsedInput, fileName), parsedOutput, fileName)
+      })
+    }
+    else {
       throw new Error('Input not found.');
     }
-    
+
   }
 };
 
 singleFileStrategy.prototype.normalizePath = function(input) {
-  let relativePath = path.normalize(input);
+  let relativePath = path_mod.normalize(input);
   return relativePath;
 };
 
@@ -41,7 +64,7 @@ singleFileStrategy.prototype.checkDir = function (path) {
   return fs.statSync(path).isDirectory();
 };
 
-function init (input, output, cipher, command) {
+function init(input, output, cipher, command) {
   return new singleFileStrategy(input, output, cipher, command);
 }
 
