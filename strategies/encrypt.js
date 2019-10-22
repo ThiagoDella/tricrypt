8d33994079ecae93204052281b474ff3b6c5df8f
hegde5 <vinayhegde2010@gmail.com>
2019-10-22 22:51:10 +0200

    Change commandline messages

--- strategies/encrypt.js
+++ strategies/encrypt.js
@@ -2,7 +2,7 @@ const chalk = require('chalk');
 const strategy = require('./strategy');
 
 function encryptStrategy(input, output, cipher) {
-  console.log('\n\n\t' + chalk.white.bgCyan.bold('File') + ' identified, applying ' + chalk.white.bgCyan.bold('Encrypting...'));
+  console.log('\n\n\t' + chalk.white.bgCyan.bold('Path') + ' identified. Now ' + chalk.white.bgCyan.bold('encrypting...'));
   strategy(input, output, cipher, 'encrypt');
 }
 
